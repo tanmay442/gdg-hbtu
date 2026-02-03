70dec6583baf361e0c80a9e352da3592f8e57af9
tanmay442 <goeltanmay442@gmail.com>
2026-02-03 15:36:23 +0000

    sitemap and robtots text file

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    sitemap({
+      hostname: 'https://gdghbtu.vercel.app',
+      dynamicRoutes: ['/events', '/Team'],
+    } as any),
   ],
 })
